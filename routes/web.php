47dd5b08bc35ba3cc44ee0b2ab250d48ddf86391
Kamal7434 <kamaldjibrilla09@gmail.com>
2026-03-30 01:54:33 +0100

    Ajout de la route setup-admin

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,20 @@ use App\Http\Controllers\StudentController;
 use App\Http\Controllers\ProgramController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\DashboardController;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
 
 Route::get('/', [DashboardController::class, 'index'])->name('dashboard')->middleware('auth');
+
+Route::get('/setup-admin', function () {
+    User::create([
+        'name' => 'Tambou',
+        'email' => 'tambou@gmail.com',
+        'password' => Hash::make('tamboubopda'),
+    ]);
+    return "Utilisateur créé !";
+});
 
 Route::middleware(['auth', 'admin'])->group(function() {
     Route::resource('students', StudentController::class);
